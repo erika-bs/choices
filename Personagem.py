8023ef78d2493046804580be2d6afb159b3cfee1
erika-bs <167558855+erika-bs@users.noreply.github.com>
2024-09-18 21:14:56 -0300

    Update Personagem.py

--- Personagem.py
+++ Personagem.py
@@ -1,27 +1,44 @@
 class Personagem():
-    def __init__(self,nome):
+    def __init__(self,nome,encontrouTecnico,vidas):
         self.nome = nome
-
-
-    def procurarSuporteTecnico(self,encontrouTecnico):
         self.encontrouTecnico = encontrouTecnico
+        self.vidas = vidas
+
+
+    def procurarSuporteTecnico(self):
 
         while True:
-            print(f"Olá, {self.nome}! Em qual sala gostaria de procurar o suporte técnico?\n a) Lab 1\n b) Lab 2\n c) Lab 3")
+            self.vidas -= 1
+            print(f"Olá, {self.nome}! Em qual sala gostaria de procurar o suporte técnico?\n a) Lab 1\n b) Lab 2\n c) Lab 3\n d) Lab 4\n e) Lab 5")
             resposta = input("")
-
-            if resposta == "a" or resposta == "A":
+    
+            if self.vidas == 0:
+                print(f"GAMER OVER,{self.nome}, você usou todas suas vidas e morreu.")
+                break
+            elif resposta == "a" or resposta == "A":
                 print(f"{self.nome}, você entrou no lab 1 e foi obrigado a assistir a aula de direito constitucional!") 
+                print(f"{self.nome}, vc ainda tem {self.vidas} vida(s)")
                 self.controuTecnico = False
                 continue
             elif resposta == "b" or resposta == "B":
                 print(f"{self.nome}, você encontrou no lab 2 e participou da confratenização da turma de psicologia!")
+                print(f"{self.nome}, vc ainda tem {self.vidas} vida(s)")
                 self.encontrouTecnico = False
                 continue
             elif resposta == "c" or resposta == "C":
                 print(f"{self.nome}, você entrou no Lab 3 e encontrou o técnico de suporte, agora você precisa convencê-lo a reiniciar o roteador!")
                 self.encontrouTecnico = True
                 break
+            elif resposta == "d" or resposta == "D":
+                print(f"{self.nome}, você entrou no Lab 4 caiu em um portal do tempo e voltou para 1897.")
+                print(f"{self.nome}, vc ainda tem {self.vidas} vida(s)")
+                self.encontrouTecnico = False
+                continue
+            elif resposta == "e" or resposta == "E":
+                print(f"{self.nome}, você entrou no Lab 5 e ganhou 2 ingressos para o rock in rio. ")
+                print(f"{self.nome}, vc ainda tem {self.vidas} vida(s)")
+                self.encontrouTecnico = False
+                continue
             else:
                 print(f"Opção inválida,{self.nome}. Tente novamente.")
 
@@ -30,6 +47,7 @@ class Personagem():
         if self.encontrouTecnico == True:
             print("Como deseja convencer o técnico a reiniciar o roteador?\n a) Oferecer ingressos para o rock in rio\n b) Dar em cima dele\n c) Ameaçá-lo de morte")
             r = input("")
+            
             if r == "a" or r == "A":
                 print("GAME OVER!\nO técnico é crente, e não aceita os ingressos.")
             elif r == "b" or r == "B":
@@ -39,4 +57,4 @@ class Personagem():
             else:
                 print("opção inválida.")
         else:
-            print("Você precisa encontrar o técnico para poder falar com ele!")
+            print("Você PERDEU!")
